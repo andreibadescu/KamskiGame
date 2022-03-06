05752a518da3f4a4e581346f95c7564d7267b376
Andrei <andreibadescu18@yahoo.com>
2022-03-06 18:44:39 +0200

    Added map loading and rendering

--- Main.cpp
+++ Main.cpp
@@ -10,26 +10,36 @@ extern "C"
 __declspec(dllexport)
 void gameInit(const GameLog& logger, InputState& inputState, GameState& gameState, GameRenderer& renderer, GameMemory& memory)
 {
-    logInfo("Test - Init");
+    gameState.map.init(MAP_SIZE_X, MAP_SIZE_Y);
+
+    for (u8 i = 0; i < TEXTURE_COUNT; ++i)
+    {
+        const texture_id textureId = renderer.loadTexture(TEXTURE_PATHS[i]);
+        gameState.map.linkTextureTagAndId(textureId, static_cast<TextureTag>(i));
+    }
+
+    gameState.map.load();
+
+    logInfo("[+] Init - done!");
 }
 
 extern "C"
 __declspec(dllexport)
 void gameInput(const GameLog& logger, const GameIO& input, InputState& inputState)
 {
-    logInfo("Test - Input");
 }
 
 extern "C"
 __declspec(dllexport)
 void gameUpdate(const GameLog& logger, GameState& gameState, const InputState& inputState, GameMemory& memory, f64& deltaTime)
 {
-    logInfo("Test - Update");
 }
 
 extern "C"
 __declspec(dllexport)
 void gameRender(const GameLog& logger, GameRenderer& renderer, const GameState& gameState, GameMemory& memory, const f64 deltaTime)
 {
-    logInfo("Test - Renderer");
+    renderer.beginBatch();
+    gameState.map.render(renderer.drawQuad);
+    renderer.endBatch();
 }
