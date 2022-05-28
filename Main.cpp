21cea39c51b16fcaecc66a916e2dba7a0f016763
Andrei <andreibadescu18@yahoo.com>
2022-05-28 17:25:44 +0300

    Integrated UI and refactored Game

--- Main.cpp
+++ Main.cpp
@@ -11,12 +11,13 @@ void loadEngine(const API& api)
 
 void Game::gameInit()
 {
+    logDebug("%f %f", buttonSize.x, buttonSize.y);
     for (u32 i = 0; i < TEXTURE_COUNT; ++i)
     {
         const texture_id textureId = ENGINE.loadTexture(TEXTURE_PATHS[i]);
         linkTextureIdByTag(textureId, static_cast<TextureTag>(i));
     }
-    startGame();
+    GAME->gameState = MAIN_MENU;
     logInfo("[+] Init - done!");
 }
 
@@ -42,11 +43,31 @@ void Game::gameInput()
     ENGINE.getMousePosition(cursorPosition.x, cursorPosition.y);
 }
 
-void Game::gameUpdate(f64& deltaTime)
+void Game::gameUpdate()
 {
     switch (gameState)
     {
     case MAIN_MENU:
+        ENGINE.setBlurWholeScreen(false);
+        if (menuButton({0.0f, -ENGINE.getScreenSize().y / 5.0f}, "EXIT"))
+        {
+            ENGINE.exit(EXIT_SUCCESS);
+        }
+        else if (menuButton({0.0f, 0.0f}, "SETTINGS"))
+        {
+            gameState = SETTINGS;
+        }
+        else if (menuButton({0.0f, +ENGINE.getScreenSize().y / 5.0f}, "PLAY"))
+        {
+            gameState = GAME_START;
+        }
+        break;
+
+    case SETTINGS:
+        if (menuButton({0.0f, 0.0f}, "GO BACK"))
+        {
+            gameState = MAIN_MENU;
+        }
         break;
 
     case GAME_PAUSED:
@@ -58,8 +79,11 @@ void Game::gameUpdate(f64& deltaTime)
 
     [[unlikely]]
     case GAME_START:
-        memset(this, 0, sizeof(Game));
-        gameInit();
+        ENGINE.setBlurWholeScreen(false);
+        memset((char*)this + offsetof(Game, disposableMemory),
+                0,
+                sizeof(Game) - offsetof(Game, disposableMemory));
+        startGame();
         break;
 
     [[likely]]
@@ -68,19 +92,7 @@ void Game::gameUpdate(f64& deltaTime)
         {
             gameState = GAME_PAUSED;
         }
-        if(ENGINE.uiButtonHoverText(
-                                    {0,0},
-                                    ENGINE.getScreenSize() / 10.0f,
-                                    AnchorPoint::C,
-                                    ID(BUTTON),
-                                    ID(BUTTON_PRESSED),
-                                    10.0f,
-                                    "PLAY AGAIN"))
-        {
-            gameState = GAME_START;
-        }            
-        
-        deltaTime = deltaTime;
+
         // check if player pressed RESTART game
         if (actionState.restart == KeyState::PRESS)
         {
@@ -89,7 +101,6 @@ void Game::gameUpdate(f64& deltaTime)
         }
         else
         {
-            updateDeltaTime((f32)deltaTime);
             velocitySystem();
             updatePlayer();
             updateEnemies();
@@ -102,15 +113,33 @@ void Game::gameUpdate(f64& deltaTime)
 
     [[unlikely]]
     case GAME_LOST:
+        ENGINE.setBlurWholeScreen(true);
+        if (menuButton({0.0f, -ENGINE.getScreenSize().y / 5.0f}, "EXIT"))
+        {
+            ENGINE.exit(EXIT_SUCCESS);
+        }
+        else if (menuButton({0.0f, 0.0f}, "GO TO MENU"))
+        {
+            gameState = MAIN_MENU;
+        }
+        else if (menuButton({0.0f, +ENGINE.getScreenSize().y / 5.0f}, "PLAY AGAIN"))
+        {
+            gameState = GAME_START;
+        }
         break;
     }
 }
 
-void Game::gameRender(const f64 deltaTime)
+void Game::gameRender()
 {
-    glm::vec3 cam = getCamera();
-    ENGINE.beginBatch(cam.x, cam.y, cam.z);
-    render();
-    renderSprites();
+    ENGINE.beginBatch(camera);
+    switch (gameState) {
+        case GAME_RUNNING:
+        case GAME_LOST:
+            render();
+            renderSprites();
+            break;
+    }
     ENGINE.endBatch();
+    ENGINE.swapClear();
 }
